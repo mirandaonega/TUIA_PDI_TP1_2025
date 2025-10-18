1e4618a561bf79dc68908ab6bcdb735405cd1c3d
Miranda Onega <mirandaonega@gmail.com>
2025-10-18 12:56:57 -0300

    Agrego archivo para problema 2

---
+++ Problema2_Validacion_de_Formulario.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Carga de formularios
+form_1 = cv2.imread('formulario_01.png')
+form_2 = cv2.imread('formulario_02.png')
+form_3 = cv2.imread('formulario_03.png')
+form_4 = cv2.imread('formulario_04.png')
+form_5 = cv2.imread('formulario_05.png')
+form_vacio = cv2.imread('formulario_vacio.png')
